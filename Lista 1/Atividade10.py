989bc4c65608c4c9f17441c5e369fd4115c7f224
pedrooo17276 <pedro_mm_rodrigues@estudante.sesisenai.org.br>
2025-05-14 11:48:19 +0000

    updete

---
+++ Lista 1/Atividade10.py
@@ -0,0 +1,7 @@
+python
+CopiarEditar
+import math
+raio = float(input("Raio da base (m): "))
+altura = float(input("Altura (m): "))
+volume = math.pi * raio ** 2 * altura
+print(f"Volume: {volume} m³")
