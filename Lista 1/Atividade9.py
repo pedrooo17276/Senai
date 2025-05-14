952058c06be409e691b388fcf4eadc87a43d8d8b
pedrooo17276 <pedro_mm_rodrigues@estudante.sesisenai.org.br>
2025-05-14 11:42:36 +0000

    update

--- Lista 1/Atividade9.py
+++ Lista 1/Atividade9.py
@@ -1,11 +1,6 @@
-litros_água = float(input("Insira a quantidade de litros de água que você deseja: "))
-litros_suco = float(input("Insira a quantidade de litros de suco que você deseja: "))
-
-proporcao_agua = 8 / 10
-proporcao_suco = 2 / 10
-litros_agua = litros_refresco * proporcao_agua
-litros_suco = litros_refresco * proporcao_suco
-
-total = (litros_agua * litros_suco) / litros_refresco
-
-print(f"A quandidade nescessarias para fazer o refresco é de: {total:.2f}")
+python
+CopiarEditar
+litros_total = float(input("Litros de refresco: "))
+agua = (8 / 10) * litros_total
+suco = (2 / 10) * litros_total
+print(f"Água: {agua}L, Suco: {suco}L")
