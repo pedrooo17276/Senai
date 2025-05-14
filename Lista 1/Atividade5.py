a619f6ddce8f8d024c4750dcad469078892fa5b5
pedrooo17276 <pedro_mm_rodrigues@estudante.sesisenai.org.br>
2025-05-14 11:45:08 +0000

    updete

--- Lista 1/Atividade5.py
+++ Lista 1/Atividade5.py
@@ -1,6 +1,3 @@
-#Atividade 5 
-#Como calcular a quantidade de novelos de lã necessários para produzir cada blusa em uma confecção, 
-#considerando que cada blusa requer uma quantidade de 120 metros de fio e que cada novelo contém 125 de metros de fio?
 lã = float(input("insira quantos metros de la você tem: "))
 metros_fio = 120
 metros_novelos = 125
