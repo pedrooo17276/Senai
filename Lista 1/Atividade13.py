3dad322a94f44a93b241867f14e2afadf849c52c
pedrooo17276 <pedro_mm_rodrigues@estudante.sesisenai.org.br>
2025-05-14 11:47:17 +0000

    updete

---
+++ Lista 1/Atividade13.py
@@ -0,0 +1,6 @@
+nota1 = float(input("Digite a primeira nota (peso 2): "))
+nota2 = float(input("Digite a segunda nota (peso 3): "))
+
+media_ponderada = (2 * nota1 + 3 * nota2) / (2 + 3)
+
+print("A média ponderada das notas é: ", media_ponderada)
