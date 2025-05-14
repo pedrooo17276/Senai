5c8d257e877b8a8eda0133f3e822c168b0f9d5c6
pedrooo17276 <pedro_mm_rodrigues@estudante.sesisenai.org.br>
2025-05-14 12:11:03 +0000

    updete

---
+++ Lista 1/Atividade24.py
@@ -0,0 +1,6 @@
+print("João recebeu seu salário de R$ 1200,00 ")
+print("mas precisa pagar C1=R$ 200,00 e C2=R$120,00) que estão atrasadas. E pagar a taxa de 2%")
+
+salario = 1200 - ((200 + 120)+(1200*0.02))
+
+print("O salario de joao é : ", salario)
