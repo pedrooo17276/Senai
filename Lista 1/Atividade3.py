21909240751244903e41d4a9251b14e7f91751c5
pedrooo17276 <pedro_mm_rodrigues@estudante.sesisenai.org.br>
2025-05-14 11:40:53 +0000

    updete

--- Lista 1/Atividade3.py
+++ Lista 1/Atividade3.py
@@ -1,7 +1,3 @@
-#Atividade 3 
-#A empresa Hipotheticus paga R$10,00 por hora normal trabalhada, e R$15,00 por hora extra. 
-#Faça um algoritmo para calcular e imprimir o salário bruto e o salário líquido de um determinado funcionário. 
-#Considere que o salário líquido é igual ao salário bruto descontando-se 10% de impostos.
 hora_n = int(input("Quantas horas normais o trabalhador trabalhou? "))
 hora_e = int(input("Quantas horas extras o trabalhador trabalhou? "))
 
