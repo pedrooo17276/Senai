703d7759ad4122c52bfbf64d9029956f36346d28
pedrooo17276 <pedro_mm_rodrigues@estudante.sesisenai.org.br>
2025-05-14 11:36:30 +0000

    updete

--- Lista 1/Atividade7.py
+++ Lista 1/Atividade7.py
@@ -1,11 +1,3 @@
-calcular_total(moedas):
-
-moedas = ('moedas_5_centavos') * 0.05 +
-moedas = ('moedas_10_centavos') * 0.10 +
-moedas = ('moedas_25_centavos') * 0.25 +
-moedas = ('moedas_50_centavos') * 0.50 +
-moedas = ('moedas_1_real') * 1.00 + 
-
 moedas = {
 moedas_1_centavo = int(input("Quantidade de moedas de 1 centavo: ")),
 moedas_5_centavos = int(input("Quantidade de moedas de 5 centavos: ")),
@@ -14,6 +6,6 @@ moedas_25_centavos = int(input("Quantidade de moedas de 25 centavos: ")),
 moedas_50_centavos = int(input("Quantidade de moedas de 50 centavos: ")),
 moedas_1_real = int(input("Quantidade de moedas de 1 real: "))
 }
-total = calcular_total(moedas):
-print(f"Valor total economizado: R$ {total:.2f}")
+total = sum(valor * qtd for valor, qtd in moedas.items())
+print(f"Valor total economizado: R$ {total}")
 
