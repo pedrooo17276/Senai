c974a8f2950da837e334a2473deadffdf3c2463c
pedrooo17276 <pedro_mm_rodrigues@estudante.sesisenai.org.br>
2025-05-14 11:57:56 +0000

    updete

---
+++ Lista 1/Atividade18.py
@@ -0,0 +1,7 @@
+base_maior = float(input("Insira o tamanho a base maior: "))
+base_menor = float(input("Insira o tamanho a base menor: "))
+altura = float(input("Insira a altura: "))
+
+A = ((base_maior + base_menor) * altura) /2
+
+print("A área do trapézio é: ",A)
